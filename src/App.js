666007d4d52b14f078fe2b8d1ead8bae40cde796
Modou-Lamin-Barrow <mlbarrow88@gmail.com>
2021-06-18 14:02:54 +0000

    Push task tracker app

---
+++ src/App.js
@@ -0,0 +1,74 @@
+
+import Header from './Components/Header'
+import Tasks from './Components/Tasks'
+import AddTask from './Components/AddTask'
+import {useState, useEffect} from 'react'
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+  
+  // Get task
+  useEffect(()=> {
+    const getTasks = async () =>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+//fetch task
+const fetchTasks = async (id) =>{
+  const res = await fetch(`http://localhost:5000/tasks,|${id}`)
+  const data = await res.json()
+
+  return data
+}
+
+  // Add Task
+  const addTask = async (task) =>{
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method : 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+        body: JSON.stringify(task)
+    }) 
+    const data = await res.json()
+    setTasks([...tasks, data])
+
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = {id, ...task }
+    // setTasks([...tasks, newTask])
+  }  
+
+  //deleting task
+   const deleteTask = async (id)=>{
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'Delete',
+      })
+
+      setTasks(tasks.filter((task) => task.id !== id))
+   }
+
+  // toggle reminder 
+  const toggleReminder = (id) =>{
+    setTasks(tasks.map((task) => task.id ===id ? {...task, reminder: !task.reminder} : task))
+  }
+  return (
+    <div className='container'>
+      <Header  onAdd={() => setShowAddTask(!showAddTask)} showAdd ={showAddTask}/> 
+     {showAddTask && <AddTask onAdd= {addTask}/>}
+     {tasks.length > 0 ? 
+     (<Tasks tasks={tasks} onDelete = {deleteTask} onToggle={toggleReminder}/>
+     ): 
+       ('No task to show')
+       }
+    </div>
+   
+  )
+}
+
+
+export default App;
